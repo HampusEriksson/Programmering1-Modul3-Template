a1ceb373c5e9e5d0a20571471e91dfbae09b2314
HampusEriksson <hampus.eriksson@ga.ntig.se>
2023-08-17 13:06:10 +0200

    Update uppg2_fruit_or_vegetable.py

---
+++ uppg2_fruit_or_vegetable.py
@@ -0,0 +1,9 @@
+"""
+Namn
+Datum
+
+Skapa en tuple med grönsaker och en tuple med frukter. 
+Användaren ska skriva in en frukt/grönsak och programmet ska säga om det är en frukt eller grönsak.
+T.ex. tomat är både frukt och grönsak, programmet måste kunna hantera detta också. 
+"""
+
